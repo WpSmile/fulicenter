b2d08edb1d766bdbdd65dc3a63541a8f087085da
WpSmile <179292126@qq.com>
2016-10-14 11:41:34 +0800

    新文件

--- app/src/main/java/cn/ucai/fulicenter/bean/PropertiesBean.java
+++ app/src/main/java/cn/ucai/fulicenter/bean/PropertiesBean.java
@@ -1,7 +1,41 @@
 package cn.ucai.fulicenter.bean;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/10/13.
  */
 public class PropertiesBean {
+
+    private ColorBean colorBean;
+
+    private AlbumsBean[] albumsBeen;
+
+    public PropertiesBean() {
+    }
+
+    public ColorBean getColorBean() {
+        return colorBean;
+    }
+
+    public void setColorBean(ColorBean colorBean) {
+        this.colorBean = colorBean;
+    }
+
+    public AlbumsBean[] getAlbumsBeen() {
+        return albumsBeen;
+    }
+
+    public void setAlbumsBeen(AlbumsBean[] albumsBeen) {
+        this.albumsBeen = albumsBeen;
+    }
+
+    @Override
+    public String toString() {
+        return "PropertiesBean{" +
+                "colorBean=" + colorBean +
+                ", albumsBeen=" + Arrays.toString(albumsBeen) +
+                '}';
+    }
 }
