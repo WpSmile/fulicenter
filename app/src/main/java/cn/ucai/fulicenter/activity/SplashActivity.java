5ec94a0de0cb154eea0e0d7f352fbbcb521e0f96
WpSmile <179292126@qq.com>
2016-10-14 15:26:54 +0800

    修改项目图表、名称，添加闪屏，闪屏2S跳转到MainActicity.修改闪屏跳转逻辑，跳转后直接退出闪屏。

---
+++ app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
@@ -0,0 +1,41 @@
+package cn.ucai.fulicenter.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.ucai.fulicenter.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final long splashTime = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                long start = System.currentTimeMillis();
+                //create db 耗时操作
+                long costTiem = System.currentTimeMillis()-start;
+                if (splashTime-costTiem>0){
+                    try {
+                        //如果耗时操作的时间小于闪屏设置的时间，线程延时
+                        Thread.sleep(splashTime-costTiem);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        }).start();
+    }
+}
