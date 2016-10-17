8d1c137a1c02446b550ab3073b83f9e3d3f2fea7
WpSmile <179292126@qq.com>
2016-10-17 10:13:47 +0800

    Butterknife的使用，底部菜单互斥事件的优化。

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -4,36 +4,61 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.fragments.NewGoodsFragment;
 import cn.ucai.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
-    RadioButton mNewGoods,mBoutique,mCategory,mCart,mPersonal;
     NewGoodsFragment newGoodsFragment;
+    @Bind(R.id.new_goods)
+    RadioButton newGoods;
+    @Bind(R.id.boutique)
+    RadioButton boutique;
+    @Bind(R.id.item_category)
+    RadioButton itemCategory;
+    @Bind(R.id.item_cart)
+    RadioButton itemCart;
+    @Bind(R.id.person)
+    RadioButton person;
 
+    int index;
+    RadioButton[] rbs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         L.i("MainActivity onCreate()");
         initView();
     }
 
 
     private void initView() {
-        mNewGoods = (RadioButton) findViewById(R.id.new_goods);
-        mBoutique = (RadioButton) findViewById(R.id.boutique);
-        mCategory = (RadioButton) findViewById(R.id.item_category);
-        mCart = (RadioButton) findViewById(R.id.item_cart);
-        mPersonal = (RadioButton) findViewById(R.id.person);
+        rbs = new RadioButton[5];
+        rbs[0] = newGoods;
+        rbs[1] = boutique;
+        rbs[2] = itemCategory;
+        rbs[3] = itemCart;
+        rbs[4] = person;
 
+        setRadioButtonStatus();
         //setDefaultFragment();
+    }
+
+    private void setRadioButtonStatus() {
+        for (int i=0;i<rbs.length;i++){
+            if (index == i){
+                rbs[i].setChecked(true);
+            }else{
+                rbs[i].setChecked(false);
+            }
+        }
     }
 
     /*private void setDefaultFragment() {
@@ -44,50 +69,31 @@ public class MainActivity extends AppCompatActivity {
         transaction.commit();
     }*/
 
-    public void onCheckedChange(View view){
+    public void onCheckedChange(View view) {
         FragmentManager manager = getSupportFragmentManager();
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.new_goods:
-                MysetCheck((RadioButton) view);
+                index = 0;
                 newGoodsFragment = new NewGoodsFragment();
                 FragmentTransaction transaction = manager.beginTransaction();
-                transaction.replace(R.id.mainLayout,newGoodsFragment);
+                transaction.replace(R.id.mainLayout, newGoodsFragment);
                 transaction.addToBackStack("stack");
                 transaction.commit();
 
                 break;
             case R.id.boutique:
-                MysetCheck((RadioButton) view);
+                index = 1;
                 break;
             case R.id.item_category:
-                MysetCheck((RadioButton) view);
+                index = 2;
                 break;
             case R.id.item_cart:
-                MysetCheck((RadioButton) view);
+                index = 3;
                 break;
             case R.id.person:
-                MysetCheck((RadioButton) view);
+                index = 4;
                 break;
         }
     }
 
-    public void MysetCheck(RadioButton v){
-        if (v!=mNewGoods){
-            mNewGoods.setChecked(false);
-        }
-        if (v!=mBoutique){
-            mBoutique.setChecked(false);
-        }
-        if (v!=mCategory){
-            mCategory.setChecked(false);
-        }
-        if (v!=mCart){
-            mCart.setChecked(false);
-        }
-        if (v!=mPersonal){
-            mPersonal.setChecked(false);
-        }
-
-
-    }
 }
