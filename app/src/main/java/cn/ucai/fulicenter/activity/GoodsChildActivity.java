5428069ceaec29ca87254a012d4aca669dabc044
WpSmile <179292126@qq.com>
2016-10-18 16:49:31 +0800

    实现商品详情图片的滚动

--- app/src/main/java/cn/ucai/fulicenter/activity/GoodsChildActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/GoodsChildActivity.java
@@ -6,6 +6,8 @@ import android.os.SystemClock;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -18,14 +20,20 @@ import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.AlbumsBean;
 import cn.ucai.fulicenter.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.bean.PropertiesBean;
+import cn.ucai.fulicenter.net.NetDao;
+import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.ImageLoader;
 import cn.ucai.fulicenter.utils.L;
+import cn.ucai.fulicenter.utils.MFGT;
 import cn.ucai.fulicenter.utils.OkHttpUtils;
 import cn.ucai.fulicenter.view.FlowIndicator;
+import cn.ucai.fulicenter.view.SlideAutoLoopView;
 
 public class GoodsChildActivity extends AppCompatActivity {
 
@@ -45,20 +53,18 @@ public class GoodsChildActivity extends AppCompatActivity {
     TextView tvgoodsName;
     @Bind(R.id.tvShopPrice)
     TextView tvShopPrice;
-    @Bind(R.id.viewPager)
-    ViewPager viewPager;
+    @Bind(R.id.slideAutoLoopView)
+    SlideAutoLoopView slideAutoLoopView;
     @Bind(R.id.myView)
     FlowIndicator myView;
     @Bind(R.id.tvGoodsBrief)
     TextView tvGoodsBrief;
     @Bind(R.id.llGoodsDetails)
-    LinearLayout llGoodsDetails;
-
-    ArrayList<GoodsDetailsBean> goodDetailList;
-    PropertiesBean[] properties;
     int goodsId;
-    int mCount,mFocus;
-    ArrayList<PropertiesBean> arraylist = new ArrayList<>();
+
+    GoodsChildActivity mContext;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,91 +74,84 @@ public class GoodsChildActivity extends AppCompatActivity {
         //商品的id
         goodsId = getIntent().getIntExtra(I.GoodsDetails.KEY_GOODS_ID, 0);
         L.e("details", "goodsid" + goodsId);
-        mCount = myView.getCount();
-        mFocus = myView.getmFocus();
 
-        //通过商品的id下载商品详情
-        downloadGoodsDetails();
-    }
-
-    private void downloadGoodsDetails() {
-        OkHttpUtils<GoodsDetailsBean> utils = new OkHttpUtils<>(this);
-        utils.url(I.SERVER_ROOT+I.REQUEST_FIND_GOOD_DETAILS)
-                .addParam(I.Goods.KEY_GOODS_ID,String.valueOf(goodsId))
-                .targetClass(GoodsDetailsBean.class)
-                .execute(new OkHttpUtils.OnCompleteListener<GoodsDetailsBean>() {
-                    @Override
-                    public void onSuccess(GoodsDetailsBean result) {
-                        L.e("result:"+result);
-                        tvGoodsEnglishName.setText(result.getGoodsEnglishName());
-                        tvgoodsName.setText(result.getGoodsName());
-                        tvShopPrice.setText(result.getCurrencyPrice());
-                        tvGoodsBrief.setText(result.getGoodsBrief());
-                        properties = result.getProperties();
-                    }
-
-                    @Override
-                    public void onError(String error) {
-
-                    }
-                });
-
+        if (goodsId==0){
+            finish();
+        }
+        mContext = this;
+        initView();
+        initData();
+        setListener();
 
     }
 
-    public void onClick(View view){
-        switch (view.getId()){
+    private void setListener() {
+
+    }
+
+    private void initData() {
+        NetDao.downloadGoodsDetails(mContext, goodsId, new OkHttpUtils.OnCompleteListener<GoodsDetailsBean>() {
+            @Override
+            public void onSuccess(GoodsDetailsBean result) {
+                if (result!=null){
+                    showGoodDetails(result);
+                }else {
+                    finish();
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                finish();
+                L.e("details,error="+error);
+                CommonUtils.showShortToast(error);
+            }
+        });
+    }
+
+    private void showGoodDetails(GoodsDetailsBean details) {
+        tvGoodsEnglishName.setText(details.getGoodsEnglishName());
+        tvgoodsName.setText(details.getGoodsName());
+        tvShopPrice.setText(details.getCurrencyPrice());
+        tvGoodsBrief.setText(details.getGoodsBrief());
+        slideAutoLoopView.startPlayLoop(myView,getAlbumImgUrl(details),getAlbumImgCount(details));
+    }
+
+    private int getAlbumImgCount(GoodsDetailsBean details) {
+        if (details.getProperties()!=null&&details.getProperties().length>0) {
+            return details.getProperties()[0].getAlbums().length;
+        }
+        return 0;
+    }
+
+    private String[] getAlbumImgUrl(GoodsDetailsBean details) {
+        String[] url = new String[]{};
+        if (details.getProperties()!=null&&details.getProperties().length>0){
+            AlbumsBean[] albums = details.getProperties()[0].getAlbums();
+            url = new String[albums.length];
+            for (int i=0;i<albums.length;i++){
+                url[i] = albums[i].getImgUrl();
+            }
+        }
+        return url;
+
+    }
+
+    private void initView() {
+
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.iamgeBack:
                 finish();
                 break;
         }
     }
 
-    class MyAsyncTask extends AsyncTask<Void,Integer,Void>{
+    /*@OnClick(R.id.iamgeBack)
+    public void onBackClick(){
+        MFGT.finish(this);
+    }*/
 
-        @Override
-        protected Void doInBackground(Void... voids) {
-            while (true){
-                mFocus = mFocus<mCount-1?mFocus+1:0;
-                publishProgress(mFocus);
-                SystemClock.sleep(2000);
-            }
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer...values){
-            myView.setFocus(values[0]);
-            viewPager.setCurrentItem(values[0]);
-        }
-    }
-
-    class MyAdapter extends PagerAdapter{
-
-        public MyAdapter(){
-            for (int i = 0;i<properties.length;i++){
-                arraylist.add(properties[i]);
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return properties.length;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view==object;
-        }
-
-       /* @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            //container.removeView(arraylist.get(position));
-
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            return super.instantiateItem(container, position);
-        }*/
-    }
 }
--- app/src/main/java/cn/ucai/fulicenter/net/netDao.java
+++ app/src/main/java/cn/ucai/fulicenter/net/netDao.java
@@ -3,6 +3,7 @@ package cn.ucai.fulicenter.net;
 import android.content.Context;
 
 import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.bean.NewGoodsBean;
 
 import cn.ucai.fulicenter.utils.OkHttpUtils;
@@ -19,6 +20,14 @@ public class NetDao {
                 .addParam(I.PAGE_ID,String.valueOf(pageId))
                 .addParam(I.PAGE_SIZE,String.valueOf(I.PAGE_ID_DEFAULT))
                 .targetClass(NewGoodsBean[].class)
+                .execute(listener);
+    }
+    public static void downloadGoodsDetails(Context context, int goodsId,
+                                            OkHttpUtils.OnCompleteListener<GoodsDetailsBean> listener){
+        OkHttpUtils utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_FIND_GOOD_DETAILS)
+                .addParam(I.GoodsDetails.KEY_GOODS_ID,String.valueOf(goodsId))
+                .targetClass(GoodsDetailsBean.class)
                 .execute(listener);
     }
 }
