958de99f881b7da7811cd2e08a80d97a155dbd5c
WpSmile <179292126@qq.com>
2016-10-20 16:44:23 +0800

    实现分类列表商品按价格或时间的升降排序

--- app/src/main/java/cn/ucai/fulicenter/activity/CategoryActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/CategoryActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -18,7 +20,6 @@ import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.adapter.GoodsAdapter;
 import cn.ucai.fulicenter.bean.NewGoodsBean;
-import cn.ucai.fulicenter.net.NetDao;
 import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.ConvertUtils;
 import cn.ucai.fulicenter.utils.L;
@@ -34,12 +35,15 @@ public class CategoryActivity extends AppCompatActivity {
     @Bind(R.id.rlGoodsDetails)
     RelativeLayout rlGoodsDetails;
     @Bind(R.id.tvPrice)
-    TextView tvPrice;
+    Button tvPrice;
     @Bind(R.id.tvTime)
-    TextView tvTime;
+    Button tvTime;
     @Bind(R.id.rvCategory)
     RecyclerView rvCategory;
 
+    boolean addTimeAsc = false;
+    boolean priceAsc = false;
+    int sortBy = I.SORT_BY_ADDTIME_DESC;
     int child_id;
     int pageId = 1;
     GoodsAdapter mAdapter;
@@ -57,19 +61,20 @@ public class CategoryActivity extends AppCompatActivity {
         mList = new ArrayList<>();
         initView();
         initData();
+
     }
 
     private void initData() {
         OkHttpUtils<NewGoodsBean[]> utils = new OkHttpUtils<>(this);
-        utils.url(I.SERVER_ROOT+I.REQUEST_FIND_GOODS_DETAILS)
-                .addParam(I.NewAndBoutiqueGoods.CAT_ID,String.valueOf(child_id))
-                .addParam(I.PAGE_ID,String.valueOf(pageId))
-                .addParam(I.PAGE_SIZE,String.valueOf(10))
+        utils.url(I.SERVER_ROOT + I.REQUEST_FIND_GOODS_DETAILS)
+                .addParam(I.NewAndBoutiqueGoods.CAT_ID, String.valueOf(child_id))
+                .addParam(I.PAGE_ID, String.valueOf(pageId))
+                .addParam(I.PAGE_SIZE, String.valueOf(10))
                 .targetClass(NewGoodsBean[].class)
                 .execute(new OkHttpUtils.OnCompleteListener<NewGoodsBean[]>() {
                     @Override
                     public void onSuccess(NewGoodsBean[] result) {
-                        if (result!=null&&result.length>0){
+                        if (result != null && result.length > 0) {
                             ArrayList<NewGoodsBean> list = ConvertUtils.array2List(result);
                             mAdapter.initData(list);
                         }
@@ -95,5 +100,28 @@ public class CategoryActivity extends AppCompatActivity {
     @OnClick(R.id.iamgeBack)
     public void onClick() {
         finish();
+    }
+
+    @OnClick({R.id.tvPrice, R.id.tvTime})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tvPrice:
+                if (priceAsc){
+                    sortBy = I.SORT_BY_PRICE_ASC;
+                }else {
+                    sortBy = I.SORT_BY_PRICE_DESC;
+                }
+                priceAsc = ! priceAsc;
+                break;
+            case R.id.tvTime:
+                if (addTimeAsc){
+                    sortBy = I.SORT_BY_ADDTIME_ASC;
+                }else {
+                    sortBy = I.SORT_BY_ADDTIME_DESC;
+                }
+                addTimeAsc = !addTimeAsc;
+                break;
+        }
+        mAdapter.setSortBy(sortBy);
     }
 }
--- app/src/main/java/cn/ucai/fulicenter/adapter/GoodsAdapter.java
+++ app/src/main/java/cn/ucai/fulicenter/adapter/GoodsAdapter.java
@@ -1,23 +1,29 @@
 package cn.ucai.fulicenter.adapter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.support.v7.widget.RecyclerView.Adapter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.bean.NewGoodsBean;
 import cn.ucai.fulicenter.utils.ImageLoader;
+import cn.ucai.fulicenter.utils.MFGT;
 
 /**
  * Created by Administrator on 2016/10/17.
@@ -25,7 +31,37 @@ import cn.ucai.fulicenter.utils.ImageLoader;
 public class GoodsAdapter extends Adapter {
     Context mContext;
     List<NewGoodsBean> mlist;
+    boolean isMore;
     String footer;
+    int sortBy = I.SORT_BY_ADDTIME_DESC;
+
+    public int getSortBy() {
+        return sortBy;
+    }
+
+    public void setSortBy(int sortBy) {
+        this.sortBy = sortBy;
+        sortBy();
+        notifyDataSetChanged();
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+        notifyDataSetChanged();
+    }
+
+    public boolean isMore() {
+        return isMore;
+    }
+
+    public void setMore(boolean more) {
+        isMore = more;
+        notifyDataSetChanged();
+    }
 
     public GoodsAdapter(Context mContext, List<NewGoodsBean> list) {
         this.mContext = mContext;
@@ -48,7 +84,7 @@ public class GoodsAdapter extends Adapter {
     public void onBindViewHolder(ViewHolder holder, int position) {
         if (getItemViewType(position)==I.TYPE_FOOTER){
             FooterViewHolder footerHolder = (FooterViewHolder) holder;
-            footerHolder.tvFooter.setText(footer);
+            footerHolder.tvFooter.setText(getFooterString());
             return;
         }else {
             GoodsViewHolder goodsViewHolder = (GoodsViewHolder) holder;
@@ -56,8 +92,15 @@ public class GoodsAdapter extends Adapter {
             goodsViewHolder.tvGoodsName.setText(goods.getGoodsName());
             goodsViewHolder.tvGoodsPrice.setText(goods.getCurrencyPrice());
             ImageLoader.downloadImg(mContext,goodsViewHolder.ivGoodsPicture,goods.getGoodsThumb());
+
+            goodsViewHolder.llNewGoods.setTag(goods.getGoodsId());
         }
     }
+
+    private int getFooterString() {
+        return isMore?R.string.load_more:R.string.no_more;
+    }
+
     @Override
     public int getItemCount() {
         return mlist == null ? 1 : mlist.size() + 1;
@@ -69,17 +112,24 @@ public class GoodsAdapter extends Adapter {
         }
         return I.TYPE_ITEM;
     }
-    static class GoodsViewHolder extends ViewHolder{
+    class GoodsViewHolder extends ViewHolder{
         @Bind(R.id.ivGoodsPicture)
         ImageView ivGoodsPicture;
         @Bind(R.id.tvGoodsName)
         TextView tvGoodsName;
         @Bind(R.id.tvGoodsPrice)
         TextView tvGoodsPrice;
+        @Bind(R.id.llNewGoods)
+        LinearLayout llNewGoods;
 
         public GoodsViewHolder(View view) {
             super(view);
             ButterKnife.bind(this, view);
+        }
+        @OnClick(R.id.llNewGoods)
+        public void onClick() {
+            int goodsId = (int) llNewGoods.getTag();
+            MFGT.gotoGoodsChildActivity((Activity) mContext,goodsId);
         }
     }
 
@@ -87,6 +137,12 @@ public class GoodsAdapter extends Adapter {
         if (mlist!=null){
             mlist.clear();
         }
+        mlist.addAll(list);
+        notifyDataSetChanged();
+    }
+
+
+    public void addData(ArrayList<NewGoodsBean> list) {
         mlist.addAll(list);
         notifyDataSetChanged();
     }
@@ -98,5 +154,34 @@ public class GoodsAdapter extends Adapter {
             super(view);
             ButterKnife.bind(this, view);
         }
+    }
+
+    //商品价格排序方法
+    private void sortBy(){
+        Collections.sort(mlist, new Comparator<NewGoodsBean>() {
+            @Override
+            public int compare(NewGoodsBean left, NewGoodsBean right) {
+                int result=0;
+                switch (sortBy){
+                    case I.SORT_BY_ADDTIME_ASC:
+                        result= (int) (Long.valueOf(left.getAddTime())-Long.valueOf(right.getAddTime()));
+                        break;
+                    case I.SORT_BY_ADDTIME_DESC:
+                        result= (int) (Long.valueOf(right.getAddTime())-Long.valueOf(left.getAddTime()));
+                        break;
+                    case I.SORT_BY_PRICE_ASC:
+                        result = getPrice(left.getCurrencyPrice())-getPrice(right.getCurrencyPrice());
+                        break;
+                    case I.SORT_BY_PRICE_DESC:
+                        result = getPrice(right.getCurrencyPrice())-getPrice(left.getCurrencyPrice());
+                        break;
+                }
+                return result;
+            }
+            private int getPrice(String prive){
+                prive = prive.substring(prive.indexOf("￥")+1);
+                return Integer.valueOf(prive);
+            }
+        });
     }
 }
