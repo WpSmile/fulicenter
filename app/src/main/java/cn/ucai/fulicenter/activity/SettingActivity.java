05945e645e47357fff5932837fa53188afa84283
WpSmile <179292126@qq.com>
2016-10-24 21:02:31 +0800

    实现设置的部分布局并实现点击返回图标返回个人中心

--- app/src/main/java/cn/ucai/fulicenter/activity/SettingActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/SettingActivity.java
@@ -1,15 +1,29 @@
 package cn.ucai.fulicenter.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.utils.MFGT;
 
 public class SettingActivity extends AppCompatActivity {
+
+    @Bind(R.id.ivBack)
+    ImageView ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.ivBack)
+    public void onClick() {
+        MFGT.finish(this);
     }
 }
