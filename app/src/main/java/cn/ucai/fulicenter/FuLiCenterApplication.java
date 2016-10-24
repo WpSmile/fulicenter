1cd8ed93248e40f77ff4489b5dc1906fb1cb7a25
WpSmile <179292126@qq.com>
2016-10-24 15:08:21 +0800

    添加SharePrefrenceUtils并获得用户名

--- app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
+++ app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
@@ -2,12 +2,33 @@ package cn.ucai.fulicenter;
 
 import android.app.Application;
 
+import cn.ucai.fulicenter.bean.User;
+
 /**
  * Created by Administrator on 2016/10/17.
  */
 public class FuLiCenterApplication extends Application {
     private static FuLiCenterApplication instance;
     public static FuLiCenterApplication application;
+
+    public static String userName;
+    private static User user;
+
+    public static User getUser() {
+        return user;
+    }
+
+    public static void setUser(User user) {
+        FuLiCenterApplication.user = user;
+    }
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static void setUserName(String userName) {
+        FuLiCenterApplication.userName = userName;
+    }
 
     @Override
     public void onCreate() {
