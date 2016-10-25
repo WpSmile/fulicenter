144d4cccb41a14a55e26ebc97c95773c39885fe5
WpSmile <179292126@qq.com>
2016-10-25 13:05:35 +0800

    在个人资料中实现用户的昵称更改（但会存在昵称不会及时更新，需重新登录才会显示修改后的昵称的问题）

--- app/src/main/java/cn/ucai/fulicenter/activity/PersonalDataActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/PersonalDataActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -15,11 +16,14 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.Result;
 import cn.ucai.fulicenter.bean.User;
 import cn.ucai.fulicenter.dao.SharePrefrenceUtils;
+import cn.ucai.fulicenter.net.NetDao;
 import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.ImageLoader;
 import cn.ucai.fulicenter.utils.MFGT;
+import cn.ucai.fulicenter.utils.OkHttpUtils;
 
 public class PersonalDataActivity extends AppCompatActivity {
 
@@ -38,12 +42,16 @@ public class PersonalDataActivity extends AppCompatActivity {
     @Bind(R.id.tvNick)
     TextView tvNick;
     User user;
+    EditText etChangeNick;
+    PersonalDataActivity mContext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_data);
         ButterKnife.bind(this);
+        mContext = this;
         initData();
     }
 
@@ -81,19 +89,43 @@ public class PersonalDataActivity extends AppCompatActivity {
 
     private void changeNick() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("修改昵称")
-                .setMessage("是否修改昵称?")
+        etChangeNick = new EditText(this);
+        builder.setTitle("设置昵称")
+                .setView(etChangeNick)
                 .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        if (etChangeNick.getText()!=null){
+                            updateUserNick();
+                        }
                     }
                 }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                CommonUtils.showLongToast("设置昵称已取消！");
             }
-        }).show();
+        }).create().show();
+    }
+
+    private void updateUserNick() {
+        NetDao.updateNick(this, user.getMuserName(), etChangeNick.getText().toString(), new OkHttpUtils.OnCompleteListener<Result>() {
+            @Override
+            public void onSuccess(Result result) {
+                if (result!=null){
+                    SharePrefrenceUtils.getInstance(mContext).saveUser(etChangeNick.getText().toString());
+                    if (user != null) {
+                        ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user),mContext, ivAvatar);
+                        tvNick.setText(user.getMuserNick());
+                    }
+                }
+                CommonUtils.showLongToast("昵称修改成功！");
+            }
+
+            @Override
+            public void onError(String error) {
+                CommonUtils.showShortToast(error);
+            }
+        });
     }
 
     private void loginout() {
@@ -104,4 +136,13 @@ public class PersonalDataActivity extends AppCompatActivity {
         }
         MFGT.finish(this);
     }
+
+    /*@Override
+    protected void onResume() {
+        super.onResume();
+        if (user != null) {
+            ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user), this, ivAvatar);
+            tvNick.setText(user.getMuserNick());
+        }
+    }*/
 }
