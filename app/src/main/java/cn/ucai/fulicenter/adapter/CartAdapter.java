b4469615d8bbb3ad4f7719e7021d3ca7447f7854
WpSmile <179292126@qq.com>
2016-10-27 17:05:15 +0800

    增加和减少购物车中商品的数量

--- app/src/main/java/cn/ucai/fulicenter/adapter/CartAdapter.java
+++ app/src/main/java/cn/ucai/fulicenter/adapter/CartAdapter.java
@@ -1,11 +1,13 @@
 package cn.ucai.fulicenter.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,9 +16,13 @@ import java.util.ArrayList;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.bean.CartBean;
+import cn.ucai.fulicenter.bean.MessageBean;
+import cn.ucai.fulicenter.net.NetDao;
 import cn.ucai.fulicenter.utils.ImageLoader;
+import cn.ucai.fulicenter.utils.OkHttpUtils;
 
 /**
  * Created by Administrator on 2016/10/27.
@@ -46,14 +52,22 @@ public class CartAdapter extends RecyclerView.Adapter {
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
-        CartBean cartBean = mlist.get(position);
+        final CartBean cartBean = mlist.get(position);
         ((CartViewHolder) holder).tvGoodsNum.setText("(" + cartBean.getCount() + ")");
 
         ((CartViewHolder) holder).tvGoodsName.setText(cartBean.getGoods().getGoodsName());
         ((CartViewHolder) holder).tvprice.setText(cartBean.getGoods().getCurrencyPrice());
         ((CartViewHolder) holder).idCheckbox1.setChecked(false);
         ImageLoader.downloadImg(mContext, ((CartViewHolder) holder).ivGoodsPicture, cartBean.getGoods().getGoodsThumb());
+        ((CartViewHolder) holder).idCheckbox1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean b) {
+                cartBean.setChecked(b);
+                mContext.sendBroadcast(new Intent(I.BROADCASE_UPDATE_CART));
+            }
+        });
 
+        ((CartViewHolder) holder).ivAddCart.setTag(position);
 
     }
 
@@ -96,12 +110,61 @@ public class CartAdapter extends RecyclerView.Adapter {
 
         @OnClick({R.id.id_checkbox1, R.id.iv_add_cart, R.id.iv_delete_cart})
         public void onClick(View view) {
+            final int position = (int) ivAddCart.getTag();
+            CartBean cart = mlist.get(position);
+
             switch (view.getId()) {
-                case R.id.id_checkbox1:
-                    break;
                 case R.id.iv_add_cart:
+
+                    NetDao.updateCart(mContext, cart.getId(), cart.getCount() + 1, new OkHttpUtils.OnCompleteListener<MessageBean>() {
+                        @Override
+                        public void onSuccess(MessageBean result) {
+                            if (result!=null&&result.isSuccess()){
+                                mlist.get(position).setCount(mlist.get(position).getCount()+1);
+                                mContext.sendBroadcast(new Intent(I.BROADCASE_UPDATE_CART));
+                                tvGoodsNum.setText("("+mlist.get(position).getCount()+")");
+                            }
+                        }
+
+                        @Override
+                        public void onError(String error) {
+
+                        }
+                    });
+
                     break;
                 case R.id.iv_delete_cart:
+
+                    if (cart.getCount()>1) {
+                        NetDao.updateCart(mContext, cart.getId(), cart.getCount() - 1, new OkHttpUtils.OnCompleteListener<MessageBean>() {
+                            @Override
+                            public void onSuccess(MessageBean result) {
+                                if (result!=null&&result.isSuccess()){
+                                    mlist.get(position).setCount(mlist.get(position).getCount()-1);
+                                    mContext.sendBroadcast(new Intent(I.BROADCASE_UPDATE_CART));
+                                    tvGoodsNum.setText("("+mlist.get(position).getCount()+")");
+                                }
+                            }
+
+                            @Override
+                            public void onError(String error) {
+
+                            }
+                        });
+                    }else {
+                        NetDao.delCart(mContext, cart.getId(), new OkHttpUtils.OnCompleteListener<MessageBean>() {
+                            @Override
+                            public void onSuccess(MessageBean result) {
+
+                            }
+
+                            @Override
+                            public void onError(String error) {
+
+                            }
+                        });
+                    }
+
                     break;
             }
         }
